1ac25e8f5441720f5d78e0150161feed6d71cdef
KimJiyoon <jjuni28@naver.com>
2023-05-08 16:47:04 +0900

    [feat] CPP09 ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,17 @@
+#include "RPN.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		std::cout << "[Usage]: ./RPN expression" << std::endl;
+		return 1;
+	}
+
+	try {
+		RPN rpn(argv[1]);
+		rpn.splitExpr();
+	} catch (std::exception e) {
+		std::cout << "Error" << std::endl;
+	}
+	return 0;
+}
